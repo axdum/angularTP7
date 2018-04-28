a827031976850917c1050782ae70fab9a64a8ffb
axdum <axel.dumont3@gmail.com>
2018-04-28 02:37:24 +0200

    ajout projet vierge

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myConsoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the myConsoApp
+ */
+angular.module('myConsoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myConsoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the myConsoApp
+ */
+angular.module('myConsoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
